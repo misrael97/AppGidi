2974c12d3ddb2aa5c1cec74f7155cd2224f08e69
misrael97 <moromoroni123@gmail.com>
2025-06-23 09:09:44 -0600

    segundo commit

--- app/src/main/java/com/example/appgidi/LoginActivity.java
+++ app/src/main/java/com/example/appgidi/LoginActivity.java
@@ -2,12 +2,24 @@ package com.example.appgidi;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Button;
+
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, HorarioActivity.class);
+            startActivity(intent);
+        });
+
+
 
         // Lógica de login vendrá después...
     }
