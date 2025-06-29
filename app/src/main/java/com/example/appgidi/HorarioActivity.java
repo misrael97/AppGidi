ca6e1b7155a7a6576d53ce54e2baf7e43c3ffd43
misrael97 <moromoroni123@gmail.com>
2025-06-29 17:49:22 -0600

    septimo commit(vistas horario,asistetencia,calificaciones)

--- app/src/main/java/com/example/appgidi/HorarioActivity.java
+++ app/src/main/java/com/example/appgidi/HorarioActivity.java
@@ -1,5 +1,6 @@
 package com.example.appgidi;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HorarioActivity extends AppCompatActivity {
 
@@ -26,6 +29,8 @@ public class HorarioActivity extends AppCompatActivity {
 
         // Activar comportamiento de selección para los botones de día
         setupDayButtons();
+        setupBottomNav(R.id.nav_horarios);
+
     }
 
     private void setupDayButtons() {
@@ -57,4 +62,29 @@ public class HorarioActivity extends AppCompatActivity {
         // Establecer por defecto el botón Lunes como seleccionado
         dayButtons[0].performClick();
     }
+
+    private void setupBottomNav(int selectedItemId) {
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(selectedItemId);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == selectedItemId) return true;
+
+            if (id == R.id.nav_horarios) {
+                startActivity(new Intent(this, HorarioActivity.class));
+            } else if (id == R.id.nav_asistencias) {
+                startActivity(new Intent(this, AsistenciaActivity.class));
+            } else if (id == R.id.nav_calificaciones) {
+                startActivity(new Intent(this, CalificacionesActivity.class));
+            }
+
+            finish(); // Finaliza la actual para evitar stack de Activities
+            return true;
+        });
+    }
+
+
+
 }
